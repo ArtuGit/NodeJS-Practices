36470105089542b84286a1ee48419e0d7dad2f29
Artem L <artuon@gmail.com>
2022-02-06 20:44:02 +0200

    Rabbit MQ base

--- Bull producer-consumer example/config.js
+++
@@ -1,7 +0,0 @@
-const Bull = require('bull')
-
-const connectQueue = (name) => new Bull(name, {
-  redis: { port: process.env.REDIS_PORT, host: process.env.REDIS_HOST }
-})
-
-module.exports = { connectQueue }
