c2ea52ee793e8aaa62b8080dd1d9fb564188b896
Artem L <artuon@gmail.com>
2022-02-07 17:45:26 +0200

    cluster

---
+++ cluster/example1/index.js
@@ -0,0 +1,10 @@
+var cluster = require('cluster');
+
+if (cluster.isWorker) {
+  console.log('I am a worker');
+} else {
+  console.log('I am a master');
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+}
