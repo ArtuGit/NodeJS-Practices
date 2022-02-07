4dee9f954a549464d289f1b6c95859d1cc854f42
Artem L <artuon@gmail.com>
2022-02-07 17:10:54 +0200

    worker2

---
+++ workers/example2/thread1.js
@@ -0,0 +1,21 @@
+const { Worker, isMainThread, parentPort } = require('worker_threads');
+
+if(isMainThread) {
+  console.log('main thread start...');
+  const worker = new Worker(__filename);
+  worker.on('message', (msg) => {
+    console.log(`Worker: ${msg}`);
+  });
+  console.log("doing some random work in main thread..!!");
+}else{
+  parentPort.postMessage('hello from worker thread');
+  cpuIntensiveTask(1000);
+  parentPort.postMessage('i am working');
+  cpuIntensiveTask(1000);
+  parentPort.postMessage('task is done..!!');
+}
+
+function cpuIntensiveTask(timeInSecond) {
+  const end = Date.now() + timeInSecond;
+  while (Date.now() < end) { }
+}
---
+++ workers/example2/thread2.js
@@ -0,0 +1,36 @@
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+
+if(isMainThread) {
+  console.log('main thread start...');
+
+  const worker = new Worker(__filename, {
+    workerData: {
+      prefix: 'Received message',
+      timeInSecond: 1000
+    }
+  });
+
+  worker.on('message', (msg) => {
+    console.log(`Worker: ${msg}`);
+  });
+
+  worker.postMessage('Done with my work.');
+
+  console.log("doing some random work in main thread..!!");
+
+} else {
+  parentPort.on('message', (msg) => {
+    console.log(`${workerData.prefix}: ${msg}`);
+  });
+
+  parentPort.postMessage('hello from worker thread');
+  cpuIntensiveTask(workerData.timeInSecond);
+  parentPort.postMessage('i am working');
+  cpuIntensiveTask(workerData.timeInSecond);
+  parentPort.postMessage('task is done..!!');
+}
+
+function cpuIntensiveTask(timeInSecond) {
+  const end = Date.now() + timeInSecond;
+  while (Date.now() < end) { }
+}
