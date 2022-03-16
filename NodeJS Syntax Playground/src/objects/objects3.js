fa09f233a7483fbe808277258771cd0485326f29
Artem Laptiev <artuon@gmail.com>
2022-03-16 08:39:24 +0200

    Object.getOwnPropertyDescriptor

--- NodeJS Syntax Playground/src/objects/objects3.js
+++ NodeJS Syntax Playground/src/objects/objects3.js
@@ -1,5 +1,5 @@
 const object1 = {
-  property1: 42,
+  property1: 1,
   method1: function() {
     console.log('Method')
   }
@@ -7,7 +7,14 @@ const object1 = {
 
 const descriptor1 = Object.getOwnPropertyDescriptor(object1, 'property1');
 const descriptor2 = Object.getOwnPropertyDescriptor(object1, 'method1');
-
-
 console.log(descriptor1);
 console.log(descriptor2);
+
+Object.defineProperty(object1, 'property2', {
+  value: 2,
+  enumerable: true,
+  configurable: true,
+  writable: true
+});
+
+console.log(object1)
