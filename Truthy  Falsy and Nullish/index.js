64cca874babcd2560b058ac056564d27201f96ea
Artem L <artuon@gmail.com>
2022-08-02 22:18:03 +0300

    Logical AND (&&)

--- Truthy  Falsy and Nullish/index.js
+++ Truthy  Falsy and Nullish/index.js
@@ -15,4 +15,15 @@ const truthy_values = [
 ].filter(Boolean);
 
 // Filter out falsy values and log remaining truthy values
-console.log(truthy_values);
+console.log('truthy_values',truthy_values);
+
+// Logical AND (&&)
+let result
+result = undefined && 'foo';  
+console.log('1:',result)
+result = 2 && 0;
+console.log('2:',result)
+result = 'foo:' && 4;
+console.log('3:',result)
+result = 'foo' && 4 && 5;
+console.log('4:',result)
