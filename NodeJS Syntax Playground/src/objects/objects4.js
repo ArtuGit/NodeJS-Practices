400e5dc42f173dae452298d549864e1638968b8f
Artu <artuon@gmail.com>
2022-12-27 21:34:42 +0200

    object proto

---
+++ NodeJS Syntax Playground/src/objects/objects4.js
@@ -0,0 +1,56 @@
+function Circle() {}
+const shape = {};
+const circle = new Circle();
+
+// Set the object prototype.
+// DEPRECATED. This is for example purposes only. DO NOT DO THIS in real code.
+shape.__proto__ = circle;
+
+// Get the object prototype
+console.log(shape.__proto__ === Circle);  // false
+console.log(shape.__proto__ === circle);  // true
+
+circle.radius = 3
+console.log({circle,shape},shape.__proto__)
+
+
+const ShapeA = function () {};
+const ShapeB = {
+  a() {
+    console.log("aaa");
+  },
+};
+
+ShapeA.prototype.__proto__ = ShapeB;
+console.log(ShapeA.prototype.__proto__); // { a: [Function: a] }
+
+const shapeA = new ShapeA();
+shapeA.a(); // aaa
+console.log(ShapeA.prototype === shapeA.__proto__); // true
+
+
+const ShapeC = function () {};
+const ShapeD = {
+  a() {
+    console.log("a");
+  },
+};
+
+const shapeC = new ShapeC();
+shapeC.__proto__ = ShapeD;
+shapeC.a(); // a
+console.log(ShapeC.prototype === shapeC.__proto__); // false
+
+
+function Test() {}
+Test.prototype.myName = function () {
+  console.log("myName");
+};
+
+const test = new Test();
+console.log(test.__proto__ === Test.prototype); // true
+test.myName(); // myName
+
+const obj = {};
+obj.__proto__ = Test.prototype;
+obj.myName(); // myName
