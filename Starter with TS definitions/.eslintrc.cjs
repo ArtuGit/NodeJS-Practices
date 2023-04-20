a892222b106df78d770ac15f1932bb7f99bdaefc
Artu <artuon@gmail.com>
2023-04-20 16:54:46 +0300

    Starter with TS def-s: basis

---
+++ Starter with TS definitions/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
