95d5e5c1f0d8516adba7b7a141fdead0cb761fb7
Artu <artuon@gmail.com>
2023-06-16 21:51:59 +0300

    Better ESLint/Prettier, .editorconfig

--- Simple Starter/.eslintrc.cjs
+++ Simple Starter/.eslintrc.cjs
@@ -5,13 +5,13 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb-base',
-    'plugin:prettier/recommended'
+    'eslint-config-airbnb-base',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   rules: {
+    quotes: ['warn', 'single'],
   }
 }
--- Simple Starter/src/index.js
+++ Simple Starter/src/index.js
@@ -1,2 +1,5 @@
 // eslint-disable-next-line no-console
 console.info('JS is here');
+const a = 'wlknwdon';
+// eslint-disable-next-line no-console
+console.log(a);
