b0b536afce4548fb9dee3b802d00486a68953d9e
Artu <artuon@gmail.com>
2023-06-19 21:49:04 +0300

    Iterators

---
+++ NodeJS Syntax Playground/src/iterators.js
@@ -0,0 +1,58 @@
+const range = {
+  start: 1,
+  end: 5,
+  [Symbol.iterator]() {
+    let current = this.start;
+    const end = this.end;
+
+    return {
+      next() {
+        if (current <= end) {
+          return { value: current++, done: false };
+          c;
+        } else {
+          return { done: true };
+        }
+      },
+    };
+  },
+};
+
+for (const num of range) {
+  console.log(num);
+}
+// Output: 1, 2, 3, 4, 5
+
+console.log(range);
+console.log([...range]);
+console.log(range[1]); // undefined
+
+function makeRangeIterator(start = 0, end = Infinity, step = 1) {
+  let nextIndex = start;
+  let iterationCount = 0;
+
+  const rangeIterator = {
+    next() {
+      let result;
+      if (nextIndex < end) {
+        result = { value: nextIndex, done: false };
+        nextIndex += step;
+        iterationCount++;
+        return result;
+      }
+      return { value: iterationCount, done: true };
+    },
+  };
+  return rangeIterator;
+}
+
+const it = makeRangeIterator(1, 10, 2);
+
+let result = it.next();
+while (!result.done) {
+  console.log(result.value); // 1 3 5 7 9
+  result = it.next();
+}
+
+console.log("Iterated over sequence of size:", result.value); // [5 numbers returned, that took interval in between: 0 to 10]
+console.log(result);
