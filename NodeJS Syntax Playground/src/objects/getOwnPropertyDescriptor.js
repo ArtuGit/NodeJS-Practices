1ad7a2e140a62f06cb864caf06e2872fdddcf119
Artu <artuon@gmail.com>
2023-06-19 22:55:28 +0300

    getOwnPropertyDescriptor

---
+++ NodeJS Syntax Playground/src/objects/getOwnPropertyDescriptor.js
@@ -0,0 +1,16 @@
+const person = {
+  name: 'Alice',
+  age: 30
+};
+
+const descriptor = Object.getOwnPropertyDescriptor(person, 'name');
+console.log(descriptor);
+/*
+Output:
+{
+  value: 'Alice',
+  writable: true,
+  enumerable: true,
+  configurable: true
+}
+*/
