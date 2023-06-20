f85dfeee1efbd733ded2820e727871b778e6a7e9
Artu <artuon@gmail.com>
2023-06-20 18:01:19 +0300

    hoisting

---
+++ NodeJS Syntax Playground/src/scope/hoisting.js
@@ -0,0 +1,22 @@
+function example() {
+  console.log(x); // Output: undefined
+  var x = 10;
+  console.log(x); // Output: 10
+}
+
+example();
+
+
+function example2() {
+  // console.log(x); // ReferenceError: Cannot access 'x' before initialization
+  console.log(y); // Output: undefined
+  let x = 10;
+  console.log(x); // Output: 10
+}
+
+example2();
+
+
+y = 20;
+var y;
+console.log(y); // Output: 20
