5830d15c381a0ff5d7dded75a23b617f0cbdb8a0
Artu <artuon@gmail.com>
2023-06-20 19:58:04 +0300

    Better Events

--- NodeJS Syntax Playground/src/events/events4.js
+++ NodeJS Syntax Playground/src/events/events4.js
@@ -1,24 +1,25 @@
-import {EventEmitter} from "events";
+import { EventEmitter } from "events";
 
 const eventEmitter = new EventEmitter();
 
-eventEmitter.on("myEvent", data => {
-  console.log(data, "- ON");
+eventEmitter.on("myEvent", (data) => {
+  console.log(data, "- ON1");
 });
 
 const MyEvent2Func = (data) => {
-  console.log(data, "- ON");
+  console.log(data, "- ON2");
 };
 
 eventEmitter.on("myEvent2", MyEvent2Func);
 
-eventEmitter.once("myEvent", data => {
+eventEmitter.once("myEvent", (data) => {
   console.log(data, "- ONCE");
 });
 
 eventEmitter.emit("myEvent", "Emitted Statement");
 eventEmitter.emit("myEvent", "Emitted Statement");
 eventEmitter.emit("myEvent", "Emitted Statement");
+eventEmitter.emit("myEvent2", "Emitted Statement");
 console.log(eventEmitter.eventNames());
 
 eventEmitter.removeListener("myEvent2", MyEvent2Func);
