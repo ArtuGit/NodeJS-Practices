fa07337165fb8be0e510dd5c7c49b9f50fd7c597
Artu <artuon@gmail.com>
2023-06-22 22:25:00 +0300

    More maps

---
+++ NodeJS Syntax Playground/src/maps/map1.js
@@ -0,0 +1,14 @@
+const originalMap = new Map();
+originalMap.set('key1', 'value1');
+originalMap.set('key2', 'value2');
+originalMap.set('key3', 'value3');
+originalMap.set('key4', 'value4');
+
+const filteredMap = new Map(
+  [...originalMap].filter(([key, value]) => {
+    // Filtering condition
+    return key.startsWith('key');
+  })
+);
+
+console.log(filteredMap);
