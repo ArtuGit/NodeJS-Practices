952030e3dcce3c3cbce24317d625bc25496a497b
Artu <artuon@gmail.com>
2024-05-31 20:47:55 +0200

    More scope

---
+++ NodeJS Syntax Playground/src/scope/scope2.js
@@ -0,0 +1,29 @@
+const hero = {
+  name: "Batman",
+  getName: function () {
+    return this.name;
+  },
+  getName2: () => {
+    return this.name;
+  },
+  getName3: function() {
+    const self = this;
+    const arrowFunc = () => {
+      return self.name;
+    }
+    return arrowFunc();
+  },
+};
+
+console.log(hero.getName()); // Output: Batman
+console.log(hero.getName2()); // Output: undefined
+
+const getHeroName = hero.getName;
+console.log(getHeroName()); // Output: undefined
+console.log(getHeroName.call(hero)); // Output: Batman
+console.log(getHeroName.bind(hero)()); // Output: Batman
+console.log(getHeroName.apply(hero)); // Output: Batman
+
+console.log(hero.getName2()); // Output: undefined
+console.log(hero.getName3()); // Output: Batman
+
